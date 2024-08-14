3e70517ae6ffd52fba929506896577f11ff3d7c6
Daniela Castro <dcastrog1@eafit.edu.co>
2024-08-14 15:07:32 -0500

    Cambios en destino y resultados

--- src/app/pages/destino/destino.component.ts
+++ src/app/pages/destino/destino.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DestinoService } from '../../servicios/destino.service';
 
 @Component({
   selector: 'app-destino',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './destino.component.css'
 })
 export class DestinoComponent {
+
+  constructor(public destinoServicio: DestinoService){}
+
+  // america?:string = this.destinoServicio.destinoA;
+  // europa?:string = this.destinoServicio.destinoE;
+
+  america = "Playa del Carmen";
+  europa = "Santorini"
+
+  srcA = "";
+  srcE = "";
+  datosA:string [] = [];
+  datosE:string [] = [];
+
+
+  destino(){
+    if(this.america == "Playa del Carmen"){
+
+      this.srcA = "../../../assets/img/PlayaDelCarmen.jpg"
+      this.srcE = "../../../assets/img/Santorini.jpg"
+      this.datosA = ["México","Español","Chichén-Itzá","Salbutes"];
+      this.datosE = ["Grecia","Griego","Oia","Hummus de Fava"];
+
+      
+    }
+  }
+
 
 }
